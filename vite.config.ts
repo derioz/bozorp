7d49b91117857800a485ec53e09068459ea46f02
derioz <txdavidj@gmail.com>
2026-02-16 20:12:27 -0600

    Fix: Configure for Vercel (output to dist, update vercel.json)

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
       host: '0.0.0.0',
     },
     build: {
-      outDir: 'docs',
+      outDir: 'dist',
     },
     base: '/',
     plugins: [react()],
